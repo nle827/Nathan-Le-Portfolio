2f94540eee8bc818b4c9cf061b15847dabc3ce13
nathanle8 <nathanle2002@Hotmail.com>
2025-09-30 00:35:12 -0700

    Fixing client and server side issues 4

--- frontend/src/app/components/ContactFormWrapper.tsx
+++ frontend/src/app/components/ContactFormWrapper.tsx
@@ -2,6 +2,13 @@
 
 import dynamic from "next/dynamic";
 
+type Props = {
+  topMargin?: string;
+  transparent?: boolean;
+};
+
 const ContactForm = dynamic(() => import("./ContactForm"), { ssr: false });
 
-export default ContactForm;
+export default function ContactFormWrapper({ topMargin, transparent }: Props) {
+  return <ContactForm topMargin={topMargin} transparent={transparent} />;
+}
